2c3db73f0fb1cd363408d51e67fff34dcbe46e4e
Shehroz Irfan <shehrozirfan89@gmail.com>
2023-05-13 12:43:58 +0500

    3-sending responses

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ const server = http.createServer((req, res) => {
   // Node uses a concept called event loop, actually it is a process that keeps on running as long as there are event listeners are registered. So, we registered the event createServer and it will keep running, actually node is single threaded. And this event loop help it to listen of hundred of thousands of requests.
 
   // we can unregister the event, with hard exit.
-  process.exit();
+  // process.exit();
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<title>");
+  res.write("<title>Home Page</title>");
+  res.write("<body><h1>It's my first node response</h1></body>");
+  res.write("</html>");
+
+  res.end();
 });
 
 server.listen(3000);
