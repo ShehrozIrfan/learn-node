d374a80f0f319d6284f07ca707033c1d0cbde718
Shehroz Irfan <shehrozirfan89@gmail.com>
2023-05-13 11:50:56 +0500

    2-the node lifecycle and event loop

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
+  // Event loop:
+  // Node uses a concept called event loop, actually it is a process that keeps on running as long as there are event listeners are registered. So, we registered the event createServer and it will keep running, actually node is single threaded. And this event loop help it to listen of hundred of thousands of requests.
+
+  // we can unregister the event, with hard exit.
+  process.exit();
 });
 
 server.listen(3000);
